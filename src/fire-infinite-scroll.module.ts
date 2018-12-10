7caad32dcb5593ad67d5ebf3fcd1aff3b76db8e2
nova <jose.saez@novainternet.es>
2018-12-10 12:31:22 +0100

    fix import

--- src/fire-infinite-scroll.module.ts
+++ src/fire-infinite-scroll.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
 import { FireInfiniteScroll } from './components/fire-infinite-scroll.component';
 
 @NgModule({
     declarations: [
         FireInfiniteScroll
     ],
+    imports: [IonicModule],
     exports: [
         FireInfiniteScroll
     ]
